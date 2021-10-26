1d3a837484a8a95f89d7a51219ce526bfba03fef
zulfan2882 <92872008+zulfan2882@users.noreply.github.com>
2021-10-26 14:58:02 +0700

    Add files via upload

---
+++ phyton.py
@@ -0,0 +1,11 @@
+print("Assallammualaikum")
+
+print("Let Me Introduce my self"); myname=input("my name is :")
+print("but you can call me"); name=input("")
+print("My NPM is "); NPM=input("")
+print("I Was born in"); born=input("")
+print("and my age is :"); age=input("")
+print("i am very glad if you want to invite my house in"); address=input("")
+print("So, don't forget to call me before with the number"); mynumber=input("")
+
+#Thank You
